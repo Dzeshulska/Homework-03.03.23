34319050965ef7e6470ffd26dd0e04b24f4e5529
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-25 14:37:43 +0200

    added changes

---
+++ task.py
@@ -0,0 +1,11 @@
+empty = ""
+print(len(empty))
+
+register_data = empty
+
+user_login = input("Enter your login: ")
+user_password = input("Enter your password: ")
+print(f"{user_login} {user_password}")
+
+
+
