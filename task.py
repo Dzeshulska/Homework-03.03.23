03ff1ba30d2989a0367ffe014b855222fbf9271a
Alla_Dseshulska <dzeshulska@gmail.com>
2023-02-27 14:50:17 +0200

    finish homework

--- task.py
+++ task.py
@@ -1,11 +1,19 @@
-empty = ""
-print(len(empty))
+registration = [ ]
+print(registration)
 
-register_data = empty
+registar_data = " "
+print(registar_data)
 
-user_login = input("Enter your login: ")
-user_password = input("Enter your password: ")
-print(f"{user_login} {user_password}")
+user_login = input(("Enter your login: "))
+user_password = input(("Enter your password: "))
+print((f"{user_login} {user_password}"))
 
+user_login = input(("Enter your login: "))
+if len(user_login) < 5:
+    print("Error")
 
+user_password = input((("Enter your password: ")))
+if len(user_password) > 10:
+    print("Eror")
 
+    
